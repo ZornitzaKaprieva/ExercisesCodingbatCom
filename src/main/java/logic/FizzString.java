087effd82ca12a6214dde169c7822e8b2ad95641
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-03-28 16:43:59 +0300

    FizzString added

---
+++ src/main/java/logic/FizzString.java
@@ -0,0 +1,29 @@
+package logic;
+
+/*Given a string str, if the string starts with "f" return "Fizz".
+If the string ends with "b" return "Buzz".
+If both the "f" and "b" conditions are true, return "FizzBuzz".
+In all other cases, return the string unchanged.
+fizzString("fig") → "Fizz"
+fizzString("dib") → "Buzz"
+fizzString("fib") → "FizzBuzz"*/
+public class FizzString {
+
+    public static void main(String[] args) {
+
+        System.out.println(fizzString("fig"));
+    }
+
+    public static String fizzString(String str) {
+
+        if (str.startsWith("f") && str.endsWith("b")){
+           return "FizzBuzz";
+        } else if (str.startsWith("f")){
+            return "Fizz";
+        } else if (str.endsWith("b")) {
+            return "Buzz";
+        }
+
+        return str;
+    }
+}
