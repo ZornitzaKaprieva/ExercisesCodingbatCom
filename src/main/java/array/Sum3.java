9bcdde5c85a80e942af9010810dc731a6de53e0c
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-03-31 13:59:59 +0300

    Sum3 added

---
+++ src/main/java/array/Sum3.java
@@ -0,0 +1,22 @@
+package array;
+
+/*Given an array of ints length 3,
+return the sum of all the elements.
+sum3([1, 2, 3]) → 6
+sum3([5, 11, 2]) → 18
+sum3([7, 0, 0]) → 7*/
+
+public class Sum3 {
+
+    public static void main(String[] args) {
+
+        int[] input = {7, 1, 0};
+        System.out.print(sum3(input));
+
+    }
+
+    public static int sum3(int[] nums) {
+
+        return nums[0] + nums[1] + nums[2];
+    }
+}
