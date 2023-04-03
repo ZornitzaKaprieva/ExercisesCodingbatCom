54950304af6b60ba782258df3a5f29433990c5d2
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-04-03 13:27:08 +0300

    EndsLy added

---
+++ src/main/java/string/EndsLy.java
@@ -0,0 +1,23 @@
+package string;
+
+/*Given a string, return true if it ends in "ly".
+endsLy("oddly") → true
+endsLy("y") → false
+endsLy("oddy") → false*/
+public class EndsLy {
+
+    public static void main(String[] args) {
+
+        System.out.println(endsLy("oddly"));
+    }
+
+    public static boolean endsLy(String str) {
+        if (str.length() <= 1){
+            return false;
+        }
+
+        return str.substring(str.length()-2).equals("ly");
+
+    }
+
+}
