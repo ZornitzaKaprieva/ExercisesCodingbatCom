49c1cc4fea1f43fea9f19549d1dfc46c7e637143
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-04-05 21:32:08 +0300

    SquirrelPlay added

---
+++ src/main/java/logic/SquirrelPlay.java
@@ -0,0 +1,30 @@
+package logic;
+
+/*The squirrels in Palo Alto spend most of the day playing.
+In particular, they play if the temperature is between 60 and 90 (inclusive).
+Unless it is summer, then the upper limit is 100 instead of 90.
+Given an int temperature and a boolean isSummer, return true if the squirrels play and false otherwise.
+squirrelPlay(70, false) → true
+squirrelPlay(95, false) → false
+squirrelPlay(95, true) → true*/
+public class SquirrelPlay {
+
+    public static void main(String[] args) {
+
+        System.out.println(squirrelPlay(70, false));
+    }
+
+    public static boolean squirrelPlay(int temp, boolean isSummer) {
+
+        //Solution 1:
+        return (!isSummer && (temp >= 60 && temp <= 90)) || (isSummer && (temp >= 60 && temp <= 100));
+
+        //Solution2:
+//        if ((!isSummer && (temp >= 60 && temp <= 90)) || (isSummer && (temp >= 60 && temp <= 100))){
+//            return true;
+//        } else  {
+//            return false;
+//        }
+    }
+}
+
