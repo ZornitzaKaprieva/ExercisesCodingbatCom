104542c16ab746d903b9d6db1c344780f4b24390
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-04-12 10:38:10 +0300

    CigarParty added

---
+++ src/main/java/logic/CigarParty.java
@@ -0,0 +1,33 @@
+package logic;
+
+/*When squirrels get together for a party, they like to have cigars.
+A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
+Unless it is the weekend, in which case there is no upper bound on the number of cigars.
+Return true if the party with the given values is successful, or false otherwise.
+cigarParty(30, false) → false
+cigarParty(50, false) → true
+cigarParty(70, true) → true*/
+public class CigarParty {
+
+    public static void main(String[] args) {
+
+        System.out.println(cigarParty(45, false));
+    }
+
+    public static boolean cigarParty(int cigars, boolean isWeekend) {
+
+        //solution 1:
+        return (!isWeekend && (cigars >= 40 && cigars <= 60)) || (isWeekend && cigars >= 40);
+
+        //solution 2:
+//        if (!isWeekend && (cigars >= 40 && cigars <= 60)){
+//            return true;
+//        } else if (isWeekend && cigars >= 40) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+
+    }
+
+}
