34cd79f5090b7dbcde47d36acaa6685baf7a55a8
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-11-08 09:42:17 +0200

    RotateLeft3 added

--- src/main/java/array/RotateLeft3.java
+++ src/main/java/array/RotateLeft3.java
@@ -10,6 +10,7 @@ public class RotateLeft3 {
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3};
+
         System.out.println(Arrays.toString(rotateLeft3(arr)));
     }
 
