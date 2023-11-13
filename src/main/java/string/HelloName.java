d5a61b2c36f3e890141800629a3854c4dd14415e
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-11-13 08:54:52 +0200

    HelloName added

---
+++ src/main/java/string/HelloName.java
@@ -0,0 +1,20 @@
+package string;
+
+/*Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"*/
+
+public class HelloName {
+    public static void main(String[] args) {
+
+        System.out.println(helloName("Bob"));
+    }
+
+    public static String helloName(String name) {
+
+        return "Hello " + name + "!";
+
+    }
+
+}
